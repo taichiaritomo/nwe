2721d6106653a194c1c75d85d3023fc0f96ba4df
Taichi Aritomo <taichiaritomo@gmail.com>
2020-04-09 21:12:32 -0400

    small tweaks and video url

---
+++ script.js
@@ -0,0 +1,57 @@
+document.querySelectorAll('div.video').forEach(videoContainer => {
+  let videoTitle = videoContainer.closest('article').querySelector('h3 a');
+  let video = videoContainer.querySelector('video');
+  let playButtonOverlay = videoContainer.querySelector('div.play-button-overlay');
+  let minimizeButton = videoContainer.querySelector('button.minimize-button');
+  let enlargeButton = videoContainer.querySelector('button.enlarge-button');
+  playButtonOverlay.addEventListener('click', () => { 
+    video.play();
+    video.controls = true;
+    videoContainer.classList.add('enlarged');
+    playButtonOverlay.style.display = 'none';
+    minimizeButton.style.display = 'block';
+  });
+  videoTitle.addEventListener('click', (event) => {
+    event.preventDefault();
+    video.play();
+    video.controls = true;
+    videoContainer.classList.add('enlarged');
+    playButtonOverlay.style.display = 'none';
+    minimizeButton.style.display = 'block';
+  });
+  minimizeButton.addEventListener('click', () => {
+    videoContainer.classList.remove('enlarged');
+    minimizeButton.style.display = 'none';
+    enlargeButton.style.display = 'block';
+  });
+  enlargeButton.addEventListener('click', () => {
+    videoContainer.classList.add('enlarged');
+    enlargeButton.style.display = 'none';
+    minimizeButton.style.display = 'block';
+  });
+  // video.addEventListener('pause', () => {
+  //   video.controls = false;
+  //   videoContainer.classList.remove('playing');
+  //   playButtonOverlay.style.display = 'flex';
+  // });
+});
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  const urlParams = new URLSearchParams(window.location.search.substring(1));
+  console.log(urlParams);
+  const videoParam = urlParams.get('video');
+  console.log(videoParam);
+  if (videoParam == 'play' && window.location.hash) {
+    let article = document.querySelector('article' + window.location.hash);
+    let videoContainer = article.querySelector('div.video');
+    let video = videoContainer.querySelector('video');
+    let playButtonOverlay = videoContainer.querySelector('div.play-button-overlay');
+    let minimizeButton = videoContainer.querySelector('button.minimize-button');
+    // video.play();
+    // video.controls = true;
+    videoContainer.classList.add('enlarged');
+    // playButtonOverlay.style.display = 'none';
+    minimizeButton.style.display = 'block';
+  }
+});
